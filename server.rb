4ed1945fdbcadf76c1b27b628e395d6dfafa297b
art kontreras <arturo.kontreras@gmail.com>
2014-05-09 16:30:00 -0500

    Add missing owner param in the add comment call.

--- server.rb
+++ server.rb
@@ -10,18 +10,20 @@ Octokit.configure do |c|
 end
 
 post '/approvals' do
-  approvals = Array.new
-  approvals = Marshal.load File.read(TEMP_FILE) if File.file?(TEMP_FILE)
   payload = JSON.parse(params[:payload])
-  # If comment is a thumbs up.
-  if payload['comment']['body'].include? '+1'
-    if approvals.empty? || !approvals.include?(payload['issue']['id'])
-      approvals.push(payload['issue']['id'])
-    else
-      Octokit.add_comment(payload['repository']['name'],payload['issue']['number'],'run tests')
-      approvals.delete(payload['issue']['id'])
+  if payload.include? 'comment'
+    approvals = Array.new
+    approvals = Marshal.load File.read(TEMP_FILE) if File.file?(TEMP_FILE)
+    # If comment is a thumbs up.
+    if payload['comment']['body'].include? '+1'
+      if approvals.empty? || !approvals.include?(payload['issue']['id'])
+        approvals.push(payload['issue']['id'])
+      else
+        Octokit.add_comment(payload['repository']['owner']['login'], payload['repository']['name'],payload['issue']['number'],'run tests')
+        approvals.delete(payload['issue']['id'])
+      end
+      File.open(TEMP_FILE, 'w') {|f| f.write(Marshal.dump(approvals)) }
     end
-    File.open(TEMP_FILE, 'w') {|f| f.write(Marshal.dump(approvals)) }
   end
   return true
 end
